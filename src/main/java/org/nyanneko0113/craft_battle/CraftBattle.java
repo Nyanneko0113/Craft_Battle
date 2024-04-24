6f5846b21a2eb0dfdf97d9c96922267bfd720540
Nyanneko0113 <yuuki01130201@gmail.com>
2024-04-24 23:27:53 +0900

    クラフトの処理を追加

--- src/main/java/org/nyanneko0113/craft_battle/CraftBattle.java
+++ src/main/java/org/nyanneko0113/craft_battle/CraftBattle.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.nyanneko0113.craft_battle.commands.GameStartCommand;
+import org.nyanneko0113.craft_battle.listener.PlayerCraftListener;
 import org.nyanneko0113.craft_battle.listener.PlayerJoinListener;
 import org.nyanneko0113.craft_battle.manager.GameManager;
 import org.nyanneko0113.craft_battle.manager.ScoreboardManager;
@@ -20,6 +21,7 @@ public class CraftBattle extends JavaPlugin {
         getCommand("craftbattle_start").setExecutor(new GameStartCommand());
 
         plm.registerEvents(new PlayerJoinListener(), this);
+        plm.registerEvents(new PlayerCraftListener(), this);
         ScoreboardManager.setScoreboard(0);
         ScoreboardManager.addOnlinePlayer();
         for (Player player : Bukkit.getOnlinePlayers()) {
---
+++ src/main/java/org/nyanneko0113/craft_battle/listener/PlayerCraftListener.java
@@ -0,0 +1,27 @@
+package org.nyanneko0113.craft_battle.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.nyanneko0113.craft_battle.manager.GameManager;
+import org.nyanneko0113.craft_battle.util.TextUtil;
+
+import java.util.Map;
+import java.util.Set;
+
+public class PlayerCraftListener implements Listener {
+
+    @EventHandler
+    public void onCraft(CraftItemEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        ItemStack item = event.getRecipe().getResult();
+        Set<ItemStack> craft = GameManager.getCraftMap().get(player);
+        craft.add(item);
+        player.sendMessage(TextUtil.TEXT_INFO + "新しいアイテムをクラフトしました。" +
+                "(現在" + ChatColor.YELLOW + craft.size() + "ポイント" + ChatColor.RESET + ")");
+    }
+
+}
